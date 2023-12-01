4dfb77722c28ee4f83aac1a107ee66a59178fc48
Andrew Healey <healeycodes@gmail.com>
2023-12-01 09:42:59 +0000

    Add day 1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # advent-of-code-2023
+
+Add inputs:
+
+```
+mkdir inputs
+cat "<day 1 input>" > inputs/1.txt
+```
+
+Run days:
+
+```
+python days/1.py
+```
+
+🎅
---
+++ days/1.py
@@ -0,0 +1,77 @@
+def match_number(s: str) -> str | None:
+    if s.startswith('one'):
+        return '1'
+    elif s.startswith('two'):
+        return '2'
+    elif s.startswith('three'):
+        return '3'
+    elif s.startswith('four'):
+        return '4'
+    elif s.startswith('five'):
+        return '5'
+    elif s.startswith('six'):
+        return '6'
+    elif s.startswith('seven'):
+        return '7'
+    elif s.startswith('eight'):
+        return '8'
+    elif s.startswith('nine'):
+        return '9'
+
+def part_one():
+    calibration_values: int = []
+    with open('./inputs/1.txt') as f:
+        for line in f.readlines():
+            number = ''
+
+            # first digit
+            for i in range(0, len(line)):
+                cur_digit = line[i]
+                if cur_digit.isdigit():
+                    number += cur_digit
+                    break
+            
+            # last digit
+            for i in range(len(line)-1, -1, -1):
+                cur_digit = line[i]
+                if cur_digit.isdigit():
+                    number += cur_digit
+                    break
+
+            calibration_values.append(int(number))
+    print(sum(calibration_values))
+
+def part_two():
+    calibration_values: int = []
+    with open('./inputs/1.txt') as f:
+        for line in f.readlines():
+            number = ''
+
+            # first digit
+            for i in range(0, len(line)):
+                cur_digit = line[i]
+                if cur_digit.isdigit():
+                    number += cur_digit
+                    break
+                number_match = match_number(line[i:])
+                if number_match:
+                    number += number_match
+                    break
+            
+            # last digit
+            for i in range(len(line)-1, -1, -1):
+                cur_digit = line[i]
+                if cur_digit.isdigit():
+                    number += cur_digit
+                    break
+                number_match = match_number(line[i:])
+                if number_match:
+                    number += number_match
+                    break
+
+            calibration_values.append(int(number))
+    print(sum(calibration_values))
+
+if __name__ == '__main__':
+    part_one()
+    part_two()
